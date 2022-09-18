0ddbf7b5a05dfa3ed2a77d73c3bbf7ef25d39cd2
DSegalStudent <dan.segal359@gmail.com>
2022-09-18 16:32:13 -0400

    Created Variables For the AppleTreeScript

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which AppleTree turns around
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
