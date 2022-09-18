5e7d3736f2e54280f0f671bfe6808cef84566135
DSegalStudent <dan.segal359@gmail.com>
2022-09-18 17:39:36 -0400

    Edited Basket Script
    Added code to basket script to allow the player to move the basket with the mouse

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -13,6 +13,19 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // get current screen position of mouse from input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The Camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
 }
