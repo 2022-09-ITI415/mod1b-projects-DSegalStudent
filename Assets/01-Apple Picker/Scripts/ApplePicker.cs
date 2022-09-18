bfb3857094fc8407b0d8d2789f41b07b4b958d5a
DSegalStudent <dan.segal359@gmail.com>
2022-09-18 18:26:54 -0400

    Created UI and Basket Lives
    Baskets will now be deleted if you drop an apple and UI should update correctly

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class ApplePicker : MonoBehaviour
     public float basketBottomY = -14;
 
     public float basketSpacingY = 2f;
+
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +25,32 @@ public class ApplePicker : MonoBehaviour
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
+    }
+
+    public void AppleDestroyed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        //Destroy one of the baskets
+        //Get the index of the last Basket in basketlist
+        int basketIndex = basketList.Count - 1;
+        //Get a reference to that basket gameobject
+        GameObject tBasketGO = basketList[basketIndex];
+        //Remove the Basket from the list and destroy the gameobject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Main-ApplePicker");
+        }
+            
     }
 
     // Update is called once per frame
--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Basket : MonoBehaviour
 {
+
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        //Get the text component of that gameobject
+        scoreGT = scoreGO.GetComponent<Text>();
+        //Set the starting number of points to 0
+        scoreGT.text = "0";
         
     }
 
@@ -37,6 +47,18 @@ public class Basket : MonoBehaviour
         if(collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            //Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+            //Add points for catching the apple
+            score += 100;
+            //Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
         }
     }
 }
