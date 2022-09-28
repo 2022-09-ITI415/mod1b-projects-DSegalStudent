4a8265a3405a6e959eabaae7c9fa1803e8ed3618
DSegalStudent <dan.segal359@gmail.com>
2022-09-27 20:22:07 -0400

    Instantiated the projectile in code

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter()
@@ -24,6 +33,17 @@ public class Slingshot : MonoBehaviour
     {
         print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+
+        projectile = Instantiate(prefabProjectile) as GameObject;
+
+        projectile.transform.position = launchPos;
+
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
 
